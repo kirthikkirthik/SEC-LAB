dcdc5db5bfed3d248d9c8da45990cc7997524a1f
kirthikkirthik <kirthikkirthik8675@gmail.com>
2025-07-15 09:11:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var w = 10;
+let name = 'js';
+const is = true;
+console.log(w+" "+name+" "+is);
+function add(a,b)
+{
+    console.log("this is a adding function");
+    console.log(a+b);
+}
+add(10,10);
+let x = parseInt(prompt("Enter 1 value: "));
+let y = parseInt(prompt("Enter 2 value: "));
+alert(x+y);
